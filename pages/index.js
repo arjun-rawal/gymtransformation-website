76a182b35ade662b6655e8f03cd210a037539cae
arjun-rawal <arawalarjun@gmail.com>
2023-03-21 22:34:37 -0400

    styled header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-
+import styles from '../styles/Home.module.css'
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
@@ -9,8 +9,13 @@ Amplify.configure(awsExports);
 function Home({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user.attributes.name}</h1>
-      <button onClick={signOut}>Sign out</button>
+    <div className={styles.header}>
+      <h1>Hello, {user.attributes.name}</h1>
+      
+    <div  className={styles.signOutButton}>
+    <button onClick={signOut}>Sign out</button>
+    </div>
+    </div>
     </>
   );
 }
