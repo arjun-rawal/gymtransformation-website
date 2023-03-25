6f5ad6b40995badfe68cabf8c9d95920062dee7d
arjun-rawal <arawalarjun@gmail.com>
2023-03-24 23:38:17 -0400

    changed s3 access to private

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -11,7 +11,7 @@ export type AmplifyDependentResourcesAttributes = {
         }
     },
     "storage": {
-        "s3progresspics": {
+        "s3gymtransformation1storage91e88757": {
             "BucketName": "string",
             "Region": "string"
         }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,25 @@
-import { Amplify } from 'aws-amplify';
-import styles from '../styles/Home.module.css'
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from '../src/aws-exports'
-import { Storage } from "@aws-amplify/storage"
-import Image from 'next/image';
-import React from 'react';
-import { useState } from 'react';
+import { Amplify } from "aws-amplify";
+import styles from "../styles/Home.module.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "../src/aws-exports";
+import { Storage } from "@aws-amplify/storage";
+import Image from "next/image";
+import React from "react";
+import { useState } from "react";
 
 Amplify.configure(awsExports);
 
 function Home({ signOut, user }) {
-
-
-
   const [images, setImages] = useState([]);
   const [pastImages, setPastImages] = useState(false);
-// retrieveImages();
+  // retrieveImages();
   async function handleFileSubmit(e) {
     const file = e.target.files[0];
     try {
       await Storage.put(file.name, file, {
-        contentType: "image/png", // contentType is optional
+        level: "private",
+        contentType: "image/png",
       });
     } catch (error) {
       console.log("Error uploading file: ", error);
@@ -29,46 +27,43 @@ function Home({ signOut, user }) {
     retrieveImages();
   }
 
-  async function retrieveImages(){
-    Storage.list('') // for listing ALL files without prefix, pass '' instead
+  async function retrieveImages() {
+    Storage.list("", { level: "private" }) // for listing ALL files without prefix, pass '' instead
       .then(({ results }) => getImage(results))
       .catch((err) => console.log(err));
-    async function getImage(results){
-    if (results.length>0){
-      setPastImages(true)
-    }
-    var imageList = [];
-    for (var i =0; i<results.length;i++){
-      imageList.push(await Storage.get(results[i].key));
-    }
-     // get key from Storage.list
-     setImages(imageList)
-     console.log(images);
-    console.log("ASD")
+    async function getImage(results) {
+      if (results.length > 0) {
+        setPastImages(true);
+      }
+      var imageList = [];
+      for (var i = 0; i < results.length; i++) {
+        imageList.push(await Storage.get(results[i].key, { level: "private" }));
+      }
+      console.log(results);
+      // get key from Storage.list
+      setImages(imageList);
+      console.log(images);
+      console.log("ASD");
     }
   }
 
-  
   return (
     <>
-    <div className={styles.header}>
-      <h1>Hello, {user.attributes.name}</h1>
-      
-    <div  className={styles.signOutButton}>
-    <button onClick={signOut}>Sign out</button>
-    </div>
-    </div>
+      <div className={styles.header}>
+        <h1>Hello, {user.attributes.name}</h1>
 
+        <div className={styles.signOutButton}>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      </div>
 
-    <input type="file" onChange={handleFileSubmit}/>
-    <button onClick={retrieveImages}>Load Images</button>
+      <input type="file" onChange={handleFileSubmit} />
+      <button onClick={retrieveImages}>Load Images</button>
 
-    {images[0] !=undefined &&
-    
-      images.map((image,index) => (
-        <img key={index} alt="a" src={image} width={200} height={200} /> 
-      ))    
-  }
+      {images[0] != undefined &&
+        images.map((image, index) => (
+          <img key={index} alt="a" src={image} width={200} height={200} />
+        ))}
     </>
   );
 }
